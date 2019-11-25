03abb1a7778e7cce92d48f3b873b7d9d7db62336
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-25 09:46:09 +0530

    Update

--- smartration/src/components/FirstPage.js
+++ smartration/src/components/FirstPage.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import './FirstPage.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
+
+
+//Functional comonent
+// const FirstPage = () =>{
+
+//     return (
+//     <div className="MainDiv">
+//         <img src="https://joeschmoe.io/api/v1/Aadesh" alt="Avatar" />
+//         <h1>Smart-Ration Card</h1>
+//         <input type="text" className="data" placeholder="Enter Ration Card Number" />
+//         <button type="submit"></button>
+//         </div>
+//     )}
+
+
+
+//classfull component
 class FirstPage extends Component{
+
+    show(){
+        let x=document.getElementById("data").value;
+        console.log(x);
+
+    }
     render(){
-        return <div class="MainDIv">
-        <h1>Hello {this.props.name}</h1>
-        <p>Welcome to junglebell </p>
+        return <div className="MainDiv">
+        <img src="https://joeschmoe.io/api/v1/Aadesh" alt="Avatar" />
+        <h1 className="text-center">Smart-Ration Card</h1>
+        <input type="text" className="data" id="data" placeholder="Enter Ration Card Number" />
+        <button type="submit" className="button" onClick={()=> this.show()}>submit</button>
         </div>
     }
 }
